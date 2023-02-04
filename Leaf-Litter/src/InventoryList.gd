9a444bf7b42a1e9feedd7bb1d36d27b7ab0e96e4
Parmeisan <parmeisan@gmail.com>
2023-02-04 13:04:45 -0600

    Bit of inventory

---
+++ Leaf-Litter/src/InventoryList.gd
@@ -0,0 +1,28 @@
+extends VBoxContainer
+class_name InventoryList
+
+var found_ids = []
+var found_items = []
+
+func _ready():
+	add_item(3)
+	reload()
+
+func add_item(i : int):
+	if not found_ids.contains(i):
+		found_ids.push(i)
+		var item = QuestItem.new(i)
+		found_items[i] = item
+	reload()
+
+var current_node
+func reload():
+	var template = get_node("InventoryItem") # before it's removed
+	Util.deleteExtraChildren(self, 1)
+	for f in found_ids:
+		current_node = template.duplicate()
+		node("Name").text = found_items[f].name_nature
+		add_child(current_node)
+
+func node(s):
+	return current_node.get_node(s)
